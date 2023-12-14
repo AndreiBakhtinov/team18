b6981f5ea88cb233027c9aacf405cde0bc0ce00a
Victoria <victoria.cernenchi@yahoo.de>
2023-12-14 22:46:36 +0100

    home_work

---
+++ victoria_cernenchi/Hw_6.sql
@@ -0,0 +1,11 @@
+Table CustomersServices{
+  chat_id integer
+  user_id integer
+  user_name varchar(128)
+  history text
+  is_deleted bool
+  created_at timestamp
+  reaction reaction_id
+  messege integer
+  reason integer
+}
