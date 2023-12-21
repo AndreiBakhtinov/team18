1160eb2d323c884d2671abc882ebac8999e4549c
Victoria <victoria.cernenchi@yahoo.de>
2023-12-21 18:56:58 +0100

    hw-update

--- victoria_cernenchi/HW-flighttable.sql
+++ victoria_cernenchi/HW-flighttable.sql
@@ -1,78 +1,78 @@
-https://dbdiagram.io/d/Tickets-Sales-657f65be56d8064ca0315abc
+// Use DBML to define your database structure
+// Docs: https://dbml.dbdiagram.io/docs
 
-
-Table passagers {
-  passager_id int
-  created_at timestamp
-  full_name varchar(128)
-  passport_num int
-  email varchar(64)
+Table passagers{
+  passagers_id int
+  last_name varchar(128)
+  first_name int
+  passaport_num int
+  email varchar(128)
+  age int
   telefon int
-  status varchar(64)
+  full_adress varchar(128)
+  created_at timestamp
   is_blocked bool
+
+}
+Table tickets{
+  tickets_id int
+  passagers_id int
+  flight_id int
+  seat_num int
+  created_at timestamp
+
 }
 Table flights{
-  flight_id int
-  passager_id int
-  created_at timestamp
-  airline_id int
-  dep_airport_id int
-  arr_airport_id int
-  dep_time timestamp
-  arr_time timestamp
+  flight_number int
+  airport_id int
+  passagers_id int
   seat_num int
-  is_available vool
+  arrival_time timestamp
+  arrival_date database
+  departure_time timestamp
+  departure_date database
+  departure_city varchar(64)
+  arrival_city varchar(64)
+  airplane_id int
 }
-Table seats{
+Table Booking{
+  booking_id int
+  passagers_id int
+  flight_number int
   seat_num int
-  passager_id int
-  flight_id int
-  is_booked bool
+  baggage varchar(64)
 }
-Table tickets {
-  ticket_id int
-  passager_id int
-  flight_id int
-  seat_num int
-  created_at timestamp
+Table Booking_Status{
   status varchar(64)
 }
-Table airports {
+Table airport{
   airport_id int
-  created_at timestamp
-  name varchar(128)
-  city varchar(64)
+  continent varchar(64)
   country varchar(64)
-  is_opened bool
-}
-Table airlines {
-  airline_id int
+  city varchar(64)
   created_at timestamp
-  name varchar(128)
+}
+Table Payment{
+  payment_id int
+  booking_id int
+  payment_date timestamp
+
 }
 
 
 
-Ref: "passagers"."passager_id" < "flights"."passager_id"
-
-Ref: "airlines"."airline_id" < "flights"."airline_id"
 
 
+Ref: "passagers"."passagers_id" < "flights"."passagers_id"
 
-Ref: "airports"."airport_id" < "flights"."dep_airport_id"
+Ref: "airport"."airport_id" < "flights"."airport_id"
 
-Ref: "airports"."airport_id" < "flights"."arr_airport_id"
+Ref: "tickets"."flight_id" < "flights"."flight_number"
 
-Ref: "seats"."seat_num" < "tickets"."seat_num"
+Ref: "Booking"."passagers_id" < "passagers"."passagers_id"
 
-Ref: "flights"."flight_id" < "tickets"."flight_id"
-
-Ref: "passagers"."passager_id" < "tickets"."passager_id"
-
-Ref: "passagers"."passager_id" < "seats"."passager_id"
-
-Ref: "flights"."flight_id" < "seats"."flight_id"
+Ref: "Payment"."booking_id" < "Booking"."booking_id"
 
 
 
-Ref: "seats"."seat_num" < "flights"."seat_num"
+https://dbdiagram.io/d/Copy-of-online_school-65564d533be1495787253458
